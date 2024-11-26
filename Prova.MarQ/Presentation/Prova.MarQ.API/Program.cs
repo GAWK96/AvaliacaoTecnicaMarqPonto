ccabc7535f281d50c77010296c0741a788625902
GAWK96 <guilherme.krieger@yahoo.com.br>
2024-11-26 20:17:21 -0300

    Jwt token authenticator added to program.cs

--- Prova.MarQ/Presentation/Prova.MarQ.API/Program.cs
+++ Prova.MarQ/Presentation/Prova.MarQ.API/Program.cs
@@ -4,9 +4,14 @@ using Prova.MarQ.Infra;
 using Prova.MarQ.Infra.Loader.Clocking_Loader;
 using Prova.MarQ.Infra.Loader.Company_Loader;
 using Prova.MarQ.Infra.Loader.Employee_Loader;
+using Prova.MarQ.Infra.Loader.User_Loader;
 using Prova.MarQ.Infra.Service.Clocking_Service;
 using Prova.MarQ.Infra.Service.Company_Service;
 using Prova.MarQ.Infra.Service.Employee_Service;
+using Prova.MarQ.Infra.Service.User_Service;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,6 +41,8 @@ builder.Services.AddScoped<IEmployeeLoader, EmployeeLoader>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddScoped<IClockingLoader, ClockingLoader>();
 builder.Services.AddScoped<IClockingService, ClockingService>();
+builder.Services.AddScoped<IUserLoader, UserLoader>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
@@ -55,3 +62,24 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+var key = Encoding.UTF8.GetBytes("Your_Secret_Key_Here"); // Replace with a secure key
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "YourIssuer",
+        ValidAudience = "YourAudience",
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
