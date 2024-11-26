e1109703c4c9d65d21f59c9dafa99b72557e0b46
GAWK96 <guilherme.krieger@yahoo.com.br>
2024-11-26 20:15:37 -0300

    User controller created (class and interface)

---
+++ Prova.MarQ/Presentation/Prova.MarQ.API/Controllers/User_Controller/IUserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prova.MarQ.Domain.Entities;
+
+namespace Prova.MarQ.API.Controllers.User_Controller
+{
+    public interface IUserController
+    {
+        Task<IActionResult> AddUser(User user);
+        Task<IActionResult> GetUserByName(string name);
+        Task<IActionResult> Login(string username, string password);
+    }
+}
---
+++ Prova.MarQ/Presentation/Prova.MarQ.API/Controllers/User_Controller/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prova.MarQ.Domain.Entities;
+using Prova.MarQ.Infra.Service.Employee_Service;
+using Prova.MarQ.Infra.Service.User_Service;
+
+namespace Prova.MarQ.API.Controllers.User_Controller
+{
+    public class UserController : ControllerBase, IUserController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("AddUser")]
+        public async Task<IActionResult> AddUser(User user)
+        {
+            await _userService.AddUser(user);
+            return Ok("User added successfully!");
+        }
+
+        [HttpGet("GetUserByName")]
+        public async Task<IActionResult> GetUserByName(string name)
+        {
+            var getUserByName = await _userService.GetUserByName(name);
+            return Ok(getUserByName);
+        }
+        [HttpGet("Login")]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            await _userService.UserLogin(username, password);
+            return Ok("User logged successfully");
+        }
+    }
+}
