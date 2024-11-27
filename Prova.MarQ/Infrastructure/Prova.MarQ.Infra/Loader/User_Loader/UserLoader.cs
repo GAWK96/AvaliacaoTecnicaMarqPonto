446aac81410bc4263bf838d562ecc40f3ffe7e8b
GAWK96 <guilherme.krieger@yahoo.com.br>
2024-11-26 23:09:33 -0300

    User Loader changed (password hash added)

--- Prova.MarQ/Infrastructure/Prova.MarQ.Infra/Loader/User_Loader/UserLoader.cs
+++ Prova.MarQ/Infrastructure/Prova.MarQ.Infra/Loader/User_Loader/UserLoader.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Prova.MarQ.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -11,13 +12,30 @@ namespace Prova.MarQ.Infra.Loader.User_Loader
     public class UserLoader : BaseLoader, IUserLoader
     {
         private readonly ProvaMarqDbContext _context;
+        private readonly PasswordHasher<User> _passwordHasher;
 
         public UserLoader(ProvaMarqDbContext context): base(context)
         {
             _context = context;
+            _passwordHasher = new PasswordHasher<User>();
+        }
+
+        public string HashThePassword(User user)
+        {
+            var hashedPassword =  _passwordHasher.HashPassword(user, user.Password);
+            user.Password = hashedPassword;
+            return hashedPassword;
+        }
+
+        public PasswordVerificationResult VerifyHashedPassword(User user, string password)
+        {
+            //var hashedPassword = HashThePassword(user);
+            var result =  _passwordHasher.VerifyHashedPassword(user, user.Password, password);
+            return result;
         }
         public async Task AddUser(User user)
         {
+            HashThePassword(user);
             await _context.User.AddAsync(user);
             await SaveChangesBusiness();
         }
