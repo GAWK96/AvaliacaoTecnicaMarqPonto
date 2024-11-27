90dd18285360e34aaab3d07cba21e5a47a91003a
GAWK96 <guilherme.krieger@yahoo.com.br>
2024-11-27 20:35:05 -0300

    Class entity updated in DB

---
+++ Prova.MarQ/Infrastructure/Prova.MarQ.Infra/Migrations/20241127205518_UpdateUserClass.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prova.MarQ.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "User",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccessFailedCount",
+                table: "User",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConcurrencyStamp",
+                table: "User",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "User",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailConfirmed",
+                table: "User",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LockoutEnabled",
+                table: "User",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LockoutEnd",
+                table: "User",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedEmail",
+                table: "User",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedUserName",
+                table: "User",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "User",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "User",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PhoneNumberConfirmed",
+                table: "User",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecurityStamp",
+                table: "User",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TwoFactorEnabled",
+                table: "User",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccessFailedCount",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyStamp",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "EmailConfirmed",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutEnabled",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutEnd",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedEmail",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedUserName",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumberConfirmed",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "SecurityStamp",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "TwoFactorEnabled",
+                table: "User");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "User",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
