a839422d192902f01af4393b0721b6a0c6b6a882
GAWK96 <guilherme.krieger@yahoo.com.br>
2024-11-27 20:34:27 -0300

    User entity changed

--- Prova.MarQ/Business/Prova.MarQ.Domain/Entities/User.cs
+++ Prova.MarQ/Business/Prova.MarQ.Domain/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,10 @@ using System.Threading.Tasks;
 
 namespace Prova.MarQ.Domain.Entities
 {
-    public class User
+    public class User: IdentityUser
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
+        //public int Id { get; set; }
+        //public string UserName { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
     }
