e9326fa712ab5db54f6abbc9ffc2253b0c8949aa
GAWK96 <guilherme.krieger@yahoo.com.br>
2025-08-08 23:47:05 -0300

    Company service created and moved to application

---
+++ Prova.MarQ/Application/Service/Company_Service/ICompanyService.cs
@@ -0,0 +1,18 @@
+﻿using Prova.MarQ.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova.MarQ.Infra.Service.Company_Service
+{
+    public interface ICompanyService
+    {
+        Task<Company?> FilterCompanyById(int id);
+        Task AddCompany(Company company);
+        Task UpdateCompany(Company company);
+        Task DeleteCompany(bool softDelete, int id);
+        //void CheckCompanyNull(Company company);
+    }
+}
